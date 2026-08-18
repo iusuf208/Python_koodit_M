8bd19cfee6d185e951f76d4b03cf4135dbcbb486
Malik M <yusufmurgustov@gmail.com>
2026-08-18 13:41:29 +0300

    laksyt

---
+++ laksyt/python_2.2.py
@@ -0,0 +1,4 @@
+sade = float(input("Anna ympyrän säde: "))
+
+pinta = 3.14 * sade
+print(F"Pinta-ala on {pinta}")
--- muuttujat.py
+++
@@ -1,8 +0,0 @@
-
-
-kanta = int(input("Anna suorakulmion kannan pituus: "))
-korkeus = int(input("Anna suorakulmion korkeus: "))
-
-ala = kanta * korkeus
-
-print(F"Suorakulmion pinta-ala on {ala}")

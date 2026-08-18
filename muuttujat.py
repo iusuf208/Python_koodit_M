969b417854891210f9b47d1af2befe92d5ec6c8e
Malik M <yusufmurgustov@gmail.com>
2026-08-18 11:16:45 +0300

    test

---
+++ muuttujat.py
@@ -0,0 +1,8 @@
+
+
+kanta = int(input("Anna suorakulmion kannan pituus: "))
+korkeus = int(input("Anna suorakulmion korkeus: "))
+
+ala = kanta * korkeus
+
+print(F"Suorakulmion pinta-ala on {ala}")
